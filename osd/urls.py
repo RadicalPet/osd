326c19af2653bf6d5e1ffebd2d8a74efb8d70c41
jagenau <sarah@radicalpet.net>
2017-07-26 16:51:25 +0200

    email test

--- osd/urls.py
+++ osd/urls.py
@@ -13,14 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from .views import IndexView
+from .views import IndexView, email, success
 from .business.views import IndexView as BusinessIndexView
 from .community.views import IndexView as CommunityIndexView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', IndexView.as_view(), name='home'),
     url(r'^community/', CommunityIndexView.as_view(), name='community_home'),
     url(r'^business/', BusinessIndexView.as_view(), name='business_home'),
+    url(r'^email/$', email, name='email'),
+    url(r'^success/$', success, name='success'),
 ]
